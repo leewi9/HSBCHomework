8aca5fda72b75cbeb29230a4e60bfa999f2c29cc
leewi9 <leewi9@PC>
2022-06-09 14:52:15 +0800

    token expired fix

--- AuthService/src/test/java/UtilsTest.java
+++ AuthService/src/test/java/UtilsTest.java
@@ -10,8 +10,8 @@ public class UtilsTest {
 
     @Test
     public void testCheckToken() throws UnsupportedEncodingException {
-        String now = Long.toString(System.currentTimeMillis() - (2 * 60 * 60 * 1000L));
-        String token1 = Base64.getEncoder().encodeToString(("u1").getBytes("utf-8")) + "$" + Base64.getEncoder().encodeToString(now.getBytes("utf-8"));
+        String twoHoursAgo = Long.toString(System.currentTimeMillis() - (2 * 60 * 60 * 1000L));
+        String token1 = Base64.getEncoder().encodeToString(("u1").getBytes("utf-8")) + "$" + Base64.getEncoder().encodeToString(twoHoursAgo.getBytes("utf-8"));
         boolean rst = Utils.checkToken(token1);
         assertFalse(rst);
     }
